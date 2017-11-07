ebde8c47f9f1259536b92a8a1b66754daa825c05
CG <1054609489@qq.com>
2017-11-07 15:45:24 +0800

    新疆维吾尔自治区添加<可克达拉市>（下属区含有<双河市>和<可克达拉市>）、北屯市、昆玉市

--- src/convert.js
+++ src/convert.js
@@ -3,31 +3,71 @@ const chinaData = require('./data')
 let chinaDataConvert = []
 
 // 入口
-const provinceMap = chinaData['86']
+// const provinceList = chinaData['86']
+// for (let provinceKey in provinceMap) {
+// 	let provinceItem = {
+// 		'value': provinceKey,
+// 		'name': provinceMap[provinceKey]
+// 	}
 
-for (let provinceKey in provinceMap) {
-	let provinceItem = {
-		'value': provinceKey,
-		'name': provinceMap[provinceKey]
-	}
+// 	// push某个省数据
+// 	chinaDataConvert.push(provinceItem)
 
+// 	cityMap = chinaData[provinceKey]
+
+// 	for (let cityKey in cityMap) {
+// 		let cityItem = {
+// 			'value': cityKey,
+// 			'name': cityMap[cityKey],
+// 			'parent': provinceKey
+// 		}
+// 		// push某个市数据
+// 		chinaDataConvert.push(cityItem)
+
+// 		areaMap = chinaData[cityKey]
+
+// 		for (let areaKey in areaMap) {
+// 			let areaItem = {
+// 				'value': areaKey,
+// 				'name': areaMap[areaKey],
+// 				'parent': cityKey
+// 			}
+// 			// push某个市数据
+// 			chinaDataConvert.push(areaItem)
+// 		}
+
+// 		let areaItemOther = {
+// 			'value': cityKey.slice(0,4) + '99',
+// 			'name': '其他',
+// 			'parent': cityKey
+// 		}
+// 		chinaDataConvert.push(areaItemOther)
+// 	}
+// }
+
+const provinceList = chinaData['provinceList']
+
+provinceList.forEach((provinceItem) => {
 	// push某个省数据
 	chinaDataConvert.push(provinceItem)
 
-	cityMap = chinaData[provinceKey]
+	cityMap = chinaData[provinceItem.value]
 
 	for (let cityKey in cityMap) {
 		let cityItem = {
 			'value': cityKey,
 			'name': cityMap[cityKey],
-			'parent': provinceKey
+			'parent': provinceItem.value
 		}
 		// push某个市数据
 		chinaDataConvert.push(cityItem)
 
 		areaMap = chinaData[cityKey]
 
+		let cityhasArea = false
+
 		for (let areaKey in areaMap) {
+			cityhasArea = true
 			let areaItem = {
 				'value': areaKey,
 				'name': areaMap[areaKey],
@@ -36,15 +76,23 @@ for (let provinceKey in provinceMap) {
 			// push某个市数据
 			chinaDataConvert.push(areaItem)
 		}
-
-		let areaItemOther = {
-			'value': cityKey.slice(0,4) + '99',
-			'name': '其他',
-			'parent': cityKey
+		if (cityhasArea) {
+			let areaItemOther = {
+				'value': cityKey.slice(0,4) + '99',
+				'name': '其他区',
+				'parent': cityKey
+			}
+			chinaDataConvert.push(areaItemOther)
+		} else {
+			let areaItemOther = {
+				'value': '--',
+				'name': '--',
+				'parent': cityKey
+			}
+			chinaDataConvert.push(areaItemOther)
 		}
-		chinaDataConvert.push(areaItemOther)
 	}
-}
+})
 
 fs.writeFile('../dist/china-area-array.js', 'module.exports = ' + JSON.stringify(chinaDataConvert, null, 4), 'utf8', (err) => {
 	if (err) {
