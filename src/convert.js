56cc38da9aac3e2d958c598233a95ed879ac2832
CG <1054609489@qq.com>
2017-11-07 19:48:22 +0800

    省地址重新排序

--- src/convert.js
+++ src/convert.js
@@ -2,49 +2,6 @@ const fs = require('fs')
 const chinaData = require('./data')
 let chinaDataConvert = []
 
-// 入口
-// const provinceList = chinaData['86']
-// for (let provinceKey in provinceMap) {
-// 	let provinceItem = {
-// 		'value': provinceKey,
-// 		'name': provinceMap[provinceKey]
-// 	}
-
-// 	// push某个省数据
-// 	chinaDataConvert.push(provinceItem)
-
-// 	cityMap = chinaData[provinceKey]
-
-// 	for (let cityKey in cityMap) {
-// 		let cityItem = {
-// 			'value': cityKey,
-// 			'name': cityMap[cityKey],
-// 			'parent': provinceKey
-// 		}
-// 		// push某个市数据
-// 		chinaDataConvert.push(cityItem)
-
-// 		areaMap = chinaData[cityKey]
-
-// 		for (let areaKey in areaMap) {
-// 			let areaItem = {
-// 				'value': areaKey,
-// 				'name': areaMap[areaKey],
-// 				'parent': cityKey
-// 			}
-// 			// push某个市数据
-// 			chinaDataConvert.push(areaItem)
-// 		}
-
-// 		let areaItemOther = {
-// 			'value': cityKey.slice(0,4) + '99',
-// 			'name': '其他',
-// 			'parent': cityKey
-// 		}
-// 		chinaDataConvert.push(areaItemOther)
-// 	}
-// }
-
 const provinceList = chinaData['provinceList']
 
 provinceList.forEach((provinceItem) => {
